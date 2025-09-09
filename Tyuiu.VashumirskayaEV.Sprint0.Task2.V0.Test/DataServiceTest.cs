3a36794e0346c01e83b53b024e90e822d3ec233f
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-09-09 21:13:13 +0500

    Task2

---
+++ Tyuiu.VashumirskayaEV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VashumirskayaEV.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.VashumirskayaEV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheakGetMessageValid()
+        {
+            var name = "Елизавета";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Елизавета", res);
+        }
+    }
+}
